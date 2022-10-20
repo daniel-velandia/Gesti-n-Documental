8bdc8c2dfc5c4fd3f4214415f34daccf07d2fbd2
Jhon-Rodrigurez <116046489+Jhon-Rodrigurez@users.noreply.github.com>
2022-10-19 20:21:42 -0500

    Modelos DAO
    Solo tengo hecho los de conexion y constantes

---
+++ dao/Constantes.java
@@ -0,0 +1,83 @@
+
+package com.mycompany.gestiondocumenta.dao;
+
+
+public class Constantes {
+    
+    public static final String URL="jdbc:mysql://localhost:3306/";
+    public static final String DATABASE="";
+    public static final String USER="root";
+    public static final String PASSWORD="";
+    
+    public static final String T_CARRERA="carrera";
+    public static final String TC_ID="id";
+    public static final String TC_NOMBRECARRERA="nombre de carrera";
+    
+    public static final String T_CLASEDOCUMENTO="clase de documento";
+    public static final String TCD_ID="id";
+    public static final String TCD_CLASE="clase";
+    
+    public static final String T_COMPANIA="compania";
+    public static final String TNC_NOMBRE="nombre";
+    public static final String TNC_NIT="nit";
+    public static final String TNC_ID="id";
+    public static final String TNC_DOCUMENTO="documento";
+    public static final String TNC_NOMBRES="nombres";
+    public static final String TNC_APELLIDOS="apellidos";
+    public static final String TNC_CORREO="correo electronico";
+    public static final String TNC_TELEFONO="telefono";
+    
+    public static final String T_ENTIDAD="entidad";
+    public static final String TE_ID="id";
+    public static final String TE_DOCUMENTO="documento";
+    public static final String TE_NOMBRES="nombres";
+    public static final String TE_APELLIDOS="apellidos";
+    public static final String TE_CORREO="correo electronico";
+    public static final String TE_TELEFONO="telefono";
+    
+    public static final String T_DOCUMENTISTA="documentista";
+    public static final String TD_ID="id";
+    public static final String TD_DOCUMENTO="documento";
+    public static final String TD_NOMBRES="nombres";
+    public static final String TD_APELLIDOS="apellidos";
+    public static final String TD_CORREO="correo electronico";
+    public static final String TD_TELEFONO="telefono";
+    
+    public static final String T_DOCUMENTO="documento";
+    public static final String TDO_ID="id";
+    public static final String TDO_ARCHIVOPDF="archivo PDF";
+    public static final String TDO_FECHA="fecha";
+    public static final String TDO_NUMERORADICADO="numero de radicado";
+    public static final String TDO_TIPORADICADO="tipo de radicado";
+    public static final String TDO_ASUNTO="asunto";
+    public static final String TDO_ANEXOS="anexos";
+    public static final String TDO_REQRESPUESTA="requiere respuesta";
+    public static final String TDO_CLASEDOCUMENTO="clase de documento";
+    public static final String TDO_IDREMITENTE="id del remitente";
+    public static final String TDO_IDENCARGADO="id del encargado";
+    public static final String TDO_IDRESPUESTA="id de respuesta";
+    
+    public static final String T_ENCARGADO="encargado";
+    public static final String TEO_AREA="area";
+    public static final String TEO_CARGO="cargo";
+    public static final String TEO_ID="id";
+    public static final String TEO_DOCUMENTO="documento";
+    public static final String TEO_NOMBRES="nombres";
+    public static final String TEO_APELLIDOS="apellidos";
+    public static final String TEO_CORREO="correo electronico";
+    public static final String TEO_TELEFONO="telefono";
+    
+    public static final String T_ESTUDIANTE="estudiante";
+    public static final String TEE_CARRERA="carrera";
+    public static final String TEE_SEMESTRE="semestre";
+    public static final String TEE_ID="id";
+    public static final String TEE_DOCUMENTO="documento";
+    public static final String TEE_NOMBRES="nombres";
+    public static final String TEE_APELLIDOS="apellidos";
+    public static final String TEE_CORREO="correo electronico";
+    public static final String TEE_TELEFONO="telefono";
+    
+    public static final String T_LOGIN="login de usuario";
+    public static final String TL_USERNAME="nombre de usuario";
+    public static final String TL_PASSWORD="contrasena";
+}
