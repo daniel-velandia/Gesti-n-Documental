19523ed4a4923b9691dbc9bcb2c063721fa532b0
Jhon-Rodrigurez <116046489+Jhon-Rodrigurez@users.noreply.github.com>
2022-10-20 17:48:32 -0500

    Clase Login Modificada
    Vuelvo a subir esta clase con algunas modificaciones. Le pude haber hecho en el código de la otra clase pero no sé si esta está bien.

--- src/main/java/Modelos/Login.java
+++ src/main/java/Modelos/Login.java
@@ -1,69 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modelos;
 
-/**
- *
- * @author Familia
- */
+package com.mycompany.gestiondocumenta.modelos;
+
+
 public class Login {
     
     private int id;
-    private String nombreUsuario;
-    private String contraseña;
+    private Usuario nombreUsuario;
+    private Usuario contraseña;
 
     public Login() {
     }
 
-    public Login(int id, String nombreUsuario, String contraseña) {
+    public Login(int id) {
         this.id = id;
-        this.nombreUsuario = nombreUsuario;
-        this.contraseña = contraseña;
     }
-
-    /**
-     * @return the id
-     */
+    
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the nombreUsuario
-     */
-    public String getNombreUsuario() {
+    public Usuario getNombreUsuario() {
         return nombreUsuario;
     }
 
-    /**
-     * @param nombreUsuario the nombreUsuario to set
-     */
-    public void setNombreUsuario(String nombreUsuario) {
+    public void setNombreUsuario(Usuario nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
     }
 
-    /**
-     * @return the contraseña
-     */
-    public String getContraseña() {
+    public Usuario getContraseña() {
         return contraseña;
     }
 
-    /**
-     * @param contraseña the contraseña to set
-     */
-    public void setContraseña(String contraseña) {
+    public void setContraseña(Usuario contraseña) {
         this.contraseña = contraseña;
     }
+
     
-    
+
 }
