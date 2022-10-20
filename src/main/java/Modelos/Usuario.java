7e4f817cd5d23aac26045b86dc59f19dc9bfcefc
Jhon-Rodrigurez <116046489+Jhon-Rodrigurez@users.noreply.github.com>
2022-10-20 17:43:41 -0500

    Clase Usuario
    Agregué una nueva clase Usuario porque este contiene los datos completos.

---
+++ src/main/java/Modelos/Usuario.java
@@ -0,0 +1,72 @@
+
+package com.mycompany.gestiondocumenta.modelos;
+
+public class Usuario {
+    
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String nombreUsuario;
+    private String contrasena;
+    private String confirmarContrasena;
+
+    public Usuario(int id,String nombre, String apellido, String nombreUsuario, String contrasena, String confirmarContrasena) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.confirmarContrasena = confirmarContrasena;
+    }
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getConfirmarContrasena() {
+        return confirmarContrasena;
+    }
+
+    public void setConfirmarContrasena(String confirmarContrasena) {
+        this.confirmarContrasena = confirmarContrasena;
+    }    
+}
