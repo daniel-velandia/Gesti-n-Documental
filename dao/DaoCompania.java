0f6630d4de3b76e03e8d76912d61f4c764bbf573
Jhon-Rodrigurez <116046489+Jhon-Rodrigurez@users.noreply.github.com>
2022-10-20 17:40:26 -0500

    DAOS
    Ayer había agregado unos daos pero los eliminé y vuelvo a reenviar estos.

---
+++ dao/DaoCompania.java
@@ -0,0 +1,6 @@
+
+package com.mycompany.gestiondocumenta.dao;
+
+public class DaoCompania {
+    
+}
---
+++ dao/IDaoLogin.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.gestiondocumenta.dao;
+
+import com.mycompany.gestiondocumenta.modelos.Login;
+
+
+public interface IDaoLogin {
+    
+    public boolean iniciarSesion(Login login);
+}
