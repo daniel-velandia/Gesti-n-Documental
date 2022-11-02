bb98100e13b2e0c11257c8e672e92f2754028954
daniel-velandia <est_da_velandia@fesc.edu.co>
2022-11-02 18:36:59 -0500

    creacion de controlador y arreglo de modelos

--- src/main/java/Controladores/Controlador.java
+++ src/main/java/Controladores/Controlador.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controladores;
 
 import Modelos.*;
@@ -16,10 +13,6 @@ import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Familia
- */
 public class Controlador implements ActionListener{
     
     // modelo
